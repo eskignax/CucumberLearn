7ad1e2fc31d050ade578bfb636031b2d7775f73e
alexismw <alexis@madwagon.co>
2013-08-14 15:29:29 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CucumberLearn::Application.config.secret_token = '5b17e4190be6776ac11577558a6a6e98e7df55e3109213b51d435a3d2be2406cd1f199416991857510efa13d0ea93dc183449de73135e7fd90d8e555f07b746b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CucumberLearn::Application.config.session_store :cookie_store, key: '_CucumberLearn_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CucumberLearn::Application.config.session_store :active_record_store
